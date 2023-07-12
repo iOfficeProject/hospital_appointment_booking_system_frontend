6bc06fe2b0464800625956bfd89104a08045a6b1
abhargava <abhargava@yellowtail.nl>
2023-07-12 17:20:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,88 +1,49 @@
-import "./App.css"
+import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Components/Home/Home";
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import LogIn from "./Components/LogIn/LoginForm";
-
 import Welcome from "./Components/Welcome/Welcome";
-
 import AdminPage from "./Components/Welcome/AdminPage";
-
 import AddDoctor from "./Components/Welcome/AddDoctor";
-
 import EditDoctor from "./Components/Welcome/EditDoctor";
-
 import AddHospital from "./Components/Welcome/AddHospital";
-
 import AddUser from "./Components/Welcome/AddUser";
 import EditUser from "./Components/Welcome/EditUser";
-
 import AddRole from "./Components/Welcome/AddRole";
-
 import HospitalPage from "./Components/Welcome/HospitalPage";
 import UserPage from "./Components/Welcome/UserPage";
 import EditHospital from "./Components/Welcome/EditHospital";
-import EditRole from "./Components/Welcome/EditRole";
 import RolePage from "./Components/Welcome/RolePage";
 
-
-
-
 function App() {
-
   return (
-
     <>
-
       <BrowserRouter>
-
         <Routes>
-
           {/* <Route path="/" element={<Home/>} />
 
         <Route path="/login" element={<LogIn/>}/> */}
 
           <Route path="/" element={<AdminPage />} />
-
           <Route path="/adddoctor" element={<AddDoctor />} />
-
           <Route path="/editdoctor" element={<EditDoctor />} />
-
           <Route path="/hospital" element={<HospitalPage />} />
-
           <Route path="/addhospital" element={<AddHospital />} />
-
           <Route path="/adduser" element={<AddUser />} />
           <Route path="/edituser" element={<EditUser />} />
-          <Route path="/user" element={<UserPage/>}/>
-
+          <Route path="/user" element={<UserPage />} />
           <Route path="/addrole" element={<AddRole />} />
-
           <Route path="/edithospital" element={<EditHospital />} />
-          <Route path="/editrole" element={<EditRole />} />
           <Route path="/role" element={<RolePage />} />
-
-
-
         </Routes>
-
-        {/* <AdminPage /> */}
-
       </BrowserRouter>
-
-      {/* // <Welcome /> */}
-
     </>
-
-  )
-
+  );
 }
-
-
-
 
 export default App;
