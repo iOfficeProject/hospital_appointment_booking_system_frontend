e5d6e15842ec85898e46aca4898efa93d28443de
Rohit <rohitgha@cybage.com>
2023-07-24 12:31:23 +0530

    Made changes in navbar.js fixed alignment issue

--- src/Components/Home/NavBar.js
+++ src/Components/Home/NavBar.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import classes from "./NavBar.module.css";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "./NavBar.css";
 
-function App() {
+function NavBar() {
   const navigate = useNavigate();
 
   const homeClickHandler = () => {
     navigate("/");
   };
+
   return (
-    <nav className={classes.nav}>
-      <ul>
-        <div>
-          <FaHome onClick={homeClickHandler} />
-        </div>
+    <nav className="navbar">
+      <div className="home-icon" onClick={homeClickHandler}>
+        <FaHome />
+      </div>
+      <ul className="navbar-items">
         <li>
           <Link to="/about">About</Link>
         </li>
@@ -24,7 +24,7 @@ function App() {
           <Link to="/services">Services</Link>
         </li>
         <li>
-          <button>
+          <button className="signin-button">
             <Link to="/login">SignIn</Link>
           </button>
         </li>
@@ -32,4 +32,7 @@ function App() {
     </nav>
   );
 }
-export default App;
+
+export default NavBar;
+
+
